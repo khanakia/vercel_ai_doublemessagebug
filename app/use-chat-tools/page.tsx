1869349566e52639dbbdb8acf48e98d46e767372
khanakia <khanakia@gmail.com>
2025-07-09 19:48:12 +0530

    first commit

---
+++ app/use-chat-tools/page.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import ChatInput from '@/component/chat-input';
+import { useChat } from '@ai-sdk/react';
+import { DefaultChatTransport } from 'ai';
+import { UseChatToolsMessage } from '../api/use-chat-tools/route';
+
+export default function Chat() {
+  const { messages, sendMessage, addToolResult, status } =
+    useChat<UseChatToolsMessage>({
+      transport: new DefaultChatTransport({ api: '/api/use-chat-tools' }),
+      maxSteps: 5,
+
+      // run client-side tools that are automatically executed:
+      // async onToolCall({ toolCall }) {
+      //   // artificial 2 second delay
+      //   await new Promise(resolve => setTimeout(resolve, 2000));
+
+      //   if (toolCall.toolName === 'getLocation') {
+      //     const cities = [
+      //       'New York',
+      //       'Los Angeles',
+      //       'Chicago',
+      //       'San Francisco',
+      //     ];
+      //     return cities[Math.floor(Math.random() * cities.length)];
+      //   }
+      // },
+    });
+
+  return (
+    <div className="flex flex-col py-24 mx-auto w-full max-w-md stretch">
+      {messages?.map(message => (
+        <div key={message.id} className="whitespace-pre-wrap">
+          <strong>{`${message.role}: `}</strong>
+          {message.parts.map((part, index) => {
+            switch (part.type) {
+              case 'text':
+                return <div key={index}>{part.text}</div>;
+
+              case 'step-start':
+                return index > 0 ? (
+                  <div key={index} className="text-gray-500">
+                    <hr className="my-2 border-gray-300" />
+                  </div>
+                ) : null;
+
+              case 'tool-askForConfirmation': {
+                switch (part.state) {
+                  case 'input-available':
+                    return (
+                      <div key={index} className="text-gray-500">
+                        {part.input.message}
+                        <div className="flex gap-2">
+                          <button
+                            className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
+                            onClick={() =>
+                              addToolResult({
+                                toolCallId: part.toolCallId,
+                                output: 'Yes, confirmed.',
+                              })
+                            }
+                          >
+                            Yes
+                          </button>
+                          <button
+                            className="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700"
+                            onClick={() =>
+                              addToolResult({
+                                toolCallId: part.toolCallId,
+                                output: 'No, denied',
+                              })
+                            }
+                          >
+                            No
+                          </button>
+                        </div>
+                      </div>
+                    );
+                  case 'output-available':
+                    return (
+                      <div key={index} className="text-gray-500">
+                        User said: {part.output}
+                      </div>
+                    );
+                }
+                break;
+              }
+
+             
+            }
+          })}
+          <br />
+        </div>
+      ))}
+
+      <ChatInput status={status} onSubmit={text => sendMessage({ text })} />
+    </div>
+  );
+}
